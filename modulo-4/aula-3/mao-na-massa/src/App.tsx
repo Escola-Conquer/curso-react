b8eefe6ee91f498d46daa5acac41f7d574bdd215
Anderson Santos <andersonribeiroow@gmail>
2022-09-23 19:41:33 -0300

    atualizacao modulo 4 aula 3

--- modulo-4/aula-3/mao-na-massa/src/App.tsx
+++ modulo-4/aula-3/mao-na-massa/src/App.tsx
@@ -1,16 +1,14 @@
-import {ThemeProvider} from "styled-components"
-import { GlobalStyles } from "./styles/globals"
-import {Home} from "./pages/Home"
-import {theme} from "./styles/theme"
-
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from './styles/globals';
+import { theme } from "./styles/theme";
 
 export function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <h1>Filmz</h1>
     </ThemeProvider>
   )
-  
 }
 
+export default App;
