bddf6d080931c0bf76c45694207e893f45862596
Anderson Santos <andersonribeiroow@gmail>
2022-09-23 19:56:04 -0300

    atualizacao modulo 4 aula 4

--- modulo-4/aula-4/mao-na-massa/src/App.tsx
+++ modulo-4/aula-4/mao-na-massa/src/App.tsx
@@ -1,18 +1,22 @@
-import {ThemeProvider} from "styled-components"
-import { GlobalStyles } from "./styles/globals"
-import {Home} from "./pages/Home"
-import {theme} from "./styles/theme"
-import { Header } from "./components/Header"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
+import { GlobalStyles } from './styles/globals';
+import { theme } from "./styles/theme";
 
 export function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Header />
-      <Home />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   )
-  
 }
 
+export default App;
--- modulo-4/aula-4/mao-na-massa/src/components/Header/index.tsx
+++ modulo-4/aula-4/mao-na-massa/src/components/Header/index.tsx
@@ -3,25 +3,26 @@ import Logo from "../../assets/logo.png"
 import * as Styles from "./styles"
 
 export function Header() {
-    return (
-      <Styles.Container>
-        <div>
-            <img
-              src={Logo}
-              alt="Logo de Filmz. Botão de play ao lado do texto 'Filmz'"
-            />
-            <FaHome />
-            Home
-            <FaBars />
-            Minha lista
-        </div>
-        <form className="search-wrapper">
-          <input
-            type="text"
-            placeholder="Pesquise um filme"
+  return (
+    <Styles.Container>
+      <div>
+          <img
+            src={Logo}
+            alt="Logo de Filmz. Botão de play ao lado do texto 'Filmz'"
           />
-        </form>
-      </Styles.Container>
-    )
-   }
-   
+
+          <FaHome />
+          Home
+          <FaBars />
+          Minha lista
+      </div>
+
+      <form className="search-wrapper">
+        <input
+          type="text"
+          placeholder="Pesquise um filme"
+        />
+      </form>
+    </Styles.Container>
+  )
+}
